1adef68566edf36e029d326a3a38e3d49b050b54
MARIO <mariojosebezerro@gmail.com>
2023-11-06 16:30:50 -0300

    Código final para melhorias

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -41,7 +41,7 @@ public class ContaTerminal {
                 transferir();
                 break;
             case 5:
-                listar();
+                listarContas();
                 break;
             case 6:
                 System.out.println("Obrigado por ultilizar nossa agência");
@@ -71,5 +71,88 @@ public class ContaTerminal {
 
         contasBancarias.add(conta);
         System.out.println("Sua conta foi criada com sucesso!!");
+
+        operacoes();
     }
+
+    private static Conta encontrarConta(int numeroConta) {
+        Conta conta = null;
+        if (contasBancarias.size() > 0) {
+            for (Conta corrente: contasBancarias) {
+                if (corrente.getNumeroConta() == numeroConta) {
+                    conta = corrente;
+                }                
+            }
+        }
+        return conta;
+    }
+
+    public static void depositar() {
+        System.out.println("Número da conta: ");
+        int numeroConta = input.nextInt();
+
+        Conta conta = encontrarConta(numeroConta);
+
+        if (conta != null) {
+            System.out.println("Qual o valor deseja depositar? ");
+            Double valorDeposito = input.nextDouble();
+            conta.depositar(valorDeposito); 
+            System.out.println("Valor depositado com sucesso!! ");
+        } else {
+            System.out.println("Conta não encontrada!! ");
+        }
+        operacoes();
+    }
+
+    public static void sacar() {
+        System.out.println("Número da conta: ");
+        int numeroConta = input.nextInt();
+
+        Conta conta = encontrarConta(numeroConta);
+
+        if (conta != null) {
+            System.out.println("Qual o valor deseja sacar? ");
+            Double valorSaque = input.nextDouble();
+            conta.sacar(valorSaque);
+            //System.out.println("Valor sacado com sucesso!! ");
+        } else {
+            System.out.println("Conta não encontrada!! ");
+        }
+        operacoes();
+    }
+
+    public static void transferir() {
+        System.out.println("Número da conta do remetente: ");
+        int numeroContaRemetente = input.nextInt();
+
+        Conta contaRemetente = encontrarConta(numeroContaRemetente);
+
+        if (contaRemetente != null) {
+            System.out.println("Número da conta do destinatário: ");
+            int numeroContaDestinatario = input.nextInt();
+
+            Conta contaDestinatario =  encontrarConta(numeroContaDestinatario);
+
+            if (contaDestinatario != null) {
+                System.out.println("Valor a transferir: ");
+                Double valor = input.nextDouble();
+
+                contaRemetente.transferir(contaDestinatario, valor);
+            } else {
+                System.out.println("Conta não encontrada!! ");
+            }
+            operacoes();            
+        }                
+    }
+
+    public static void listarContas() {
+          if (contasBancarias.size() > 0) {
+            for (Conta conta : contasBancarias) {
+                System.out.println(conta);
+            }
+          } else {
+            System.out.println("Não há contas cadastradas! ");
+          }
+          operacoes();
+        }
 }
